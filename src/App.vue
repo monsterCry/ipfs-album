45f5dedef6ca5bdacd89a2059f9cec5fc6df320a
monstercry <963077531@qq.com>
2022-05-11 17:25:44 +0800

    1.use etherjs 2.add the wallet connect code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <template>
   <div id="app">
+    <div>
+      <el-button @click="connectWallet()">
+        {{this.address?this.address:(this.provider?'connect metamask':'install metamask')}}
+      </el-button>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+
+  import { ethers } from "ethers";
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      address:null,
+      provider: null
+    };
+  },
+  async mounted() {
+    if(window.ethereum) {
+      this.$data.provider  = new ethers.providers.Web3Provider(window.ethereum);
+    }
+  },
+  methods:{
+    async connectWallet() {
+      if(this.address) {
+        return ;
+      }
+      if(this.provider) {
+        await this.provider.send("eth_requestAccounts", []);
+        const signer = await this.provider.getSigner();
+        let acc =  await signer.getAddress();
+        this.$data.address = acc;
+      }
+    }
+  }
 }
 </script>
 
